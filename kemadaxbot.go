6aebb930684bdd3239119ba8c9ec3da86aa2b7d6
AdamekAdam <kemadax@gmail.com>
2021-12-01 11:26:57 -0800

    log print token

--- kemadaxbot.go
+++ kemadaxbot.go
@@ -33,6 +33,8 @@ func main() {
 	bot, err := tgbotapi.NewBotAPI(token)
 	if err != nil {
 		log.WithError(err).Fatal("Something wrong with telegram token")
+		log.Printf("Telegram token: %s", token)
+
 	}
 
 	bot.Debug = true
