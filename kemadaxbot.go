e51b0c3925690ee5661cc804f9bdacaf882624e1
AdamekAdam <kemadax@gmail.com>
2021-12-01 12:16:06 -0800

    code changed to last stable version wo listenandserve panic 2

--- kemadaxbot.go
+++ kemadaxbot.go
@@ -68,14 +68,14 @@ func main() {
 	go http.ListenAndServe(":8080", nil)
 
 	for update := range updates {
-
-		log.Printf("%+v\n", update)
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Hi Buddy!")
-		if _, err := bot.Send(msg); err != nil {
-			log.Panic(err)
-		}
-
-		/*msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
+		/*
+			log.Printf("%+v\n", update)
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Hi Buddy!")
+			if _, err := bot.Send(msg); err != nil {
+				log.Panic(err)
+			}
+		*/
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
 
 		if !update.Message.IsCommand() {
 			log.Debug("Answering Hey by default")
@@ -97,7 +97,7 @@ func main() {
 		if _, err := bot.Send(msg); err != nil {
 			log.Debug("Bot sending response")
 			log.Panic(err)
-		}*/
+		}
 
 	}
 
