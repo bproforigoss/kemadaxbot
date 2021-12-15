8e3d9024b23e9152d83fa6e775d832f3640c6ca8
AdamekAdam <kemadax@gmail.com>
2021-12-15 13:35:55 -0800

    convert numbers up to 10

--- kemadaxbot.go
+++ kemadaxbot.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"strconv"
 
 	log "github.com/sirupsen/logrus"
 
@@ -15,6 +16,25 @@ func init() {
 	log.SetFormatter(&log.JSONFormatter{})
 	log.SetOutput(os.Stdout)
 
+}
+func convert(num int) string {
+	var egyes = map[int]string{
+		1:  "egy",
+		2:  "kettő",
+		3:  "három",
+		4:  "négy",
+		5:  "öt",
+		6:  "hat",
+		7:  "hét",
+		8:  "nyolc",
+		9:  "kilenc",
+		10: "tíz",
+	}
+	value, ok := egyes[num]
+	if !ok {
+		return "This value is not currently found in our database to convert"
+	}
+	return value
 }
 
 func main() {
@@ -68,13 +88,7 @@ func main() {
 	go func() { log.Panic(http.ListenAndServe(":8080", nil)) }()
 
 	for update := range updates {
-		/*
-			log.Printf("%+v\n", update)
-			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Hi Buddy!")
-			if _, err := bot.Send(msg); err != nil {
-				log.Panic(err)
-			}
-		*/
+
 		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
 
 		if !update.Message.IsCommand() {
@@ -87,7 +101,14 @@ func main() {
 			case "convert":
 				log.Debug("Converting number to text")
 				arg := update.Message.CommandArguments()
-				msg.Text = arg
+				num, err := strconv.Atoi(arg)
+				if err != nil {
+					log.Debug("/convert command parameter is not number")
+					msg.Text = "Wrong parameter, only numbers as parameters are excepted"
+				}
+				convertedNum := convert(num)
+
+				msg.Text = convertedNum
 			case "ping":
 				log.Debug("Responding pong, to /ping command")
 				msg.Text = "pong"
